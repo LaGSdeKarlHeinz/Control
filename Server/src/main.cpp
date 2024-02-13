1c70ab157f831bdd0e46f6d28279a958ab50962a
Maxime Rochat <rochat.max@gmail.com>
2024-02-13 13:25:30 +0100

    deleted build folder

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -12,11 +12,10 @@ int main(int argc, char *argv[]) {
     int port(12345);
     std::cout << "Server started" << std::endl;
     if (server.listen(QHostAddress::Any, port)) {
-            std::cout << "Server started. Listening on port" << port << std::endl;
-        } else {
-            std::cout << "Error: Failed to start server. Check if the port is already in use or permissions issue." <<std::endl;
-            
-        }
+        std::cout << "Server started. Listening on port" << port << std::endl;
+    } else {
+        std::cout << "Error: Failed to start server. Check if the port is already in use or permissions issue." <<std::endl;      
+    }
     
     return a.exec();
 }
