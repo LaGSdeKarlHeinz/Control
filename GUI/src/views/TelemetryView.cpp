8ceb6f36e7a8e11893fbf7e4b1bec9a675de962b
Maxime Rochat <rochat.max@outlook.com>
2024-04-08 18:36:22 +0200

    added card styling to telemetry view

--- GUI/src/views/TelemetryView.cpp
+++ GUI/src/views/TelemetryView.cpp
@@ -26,14 +26,32 @@ TelemetryView::TelemetryView(QWidget* parent) : QFrame(parent) {
     layout->setAlignment(Qt::AlignTop);
     
     // addField(GUI_FIELD::DISCONNECT_ACTIVE);
-    QList<GUI_FIELD> fields = {GUI_FIELD::DISCONNECT_ACTIVE, GUI_FIELD::CHAMBER_PRESSURE};
-    createSection("Test", &fields);
+    
+
+    QList<GUI_FIELD> fields = {
+        GUI_FIELD::AV_STATE
+    };
+    createSection("AV State", &fields);
+
+    fields = {
+        GUI_FIELD::GNSS_CHOICE,
+        GUI_FIELD::GNSS_LON, 
+        GUI_FIELD::GNSS_LAT,
+        GUI_FIELD::GNSS_ALT,
+        GUI_FIELD::GNSS_LON_R, 
+        GUI_FIELD::GNSS_LAT_R,
+        GUI_FIELD::GNSS_ALT_R,
+        GUI_FIELD::GNSS_VERTICAL_SPEED
+    };
+    createSection("Positioning", &fields);
 }
 
 void TelemetryView::createSection(QString title, QList<GUI_FIELD> *fields) {
     QLabel* l = new QLabel(title);
-    l->setStyleSheet(QString("font-size: 14pt; color: %1;font-weight: 400;").arg(col::primary));
+    l->setStyleSheet(QString("font-size: 14pt; color: %1;font-weight: 400;background: transparent;").arg(col::primary));
     QWidget *wrapper = new QWidget;
+    wrapper->setObjectName("wrapper");
+    wrapper->setStyleSheet(col::defaultCardStyle("wrapper"));
     QVBoxLayout* layoutField = new QVBoxLayout(wrapper);
     layoutField->addWidget(l);
     for (int i=0; i<fields->size(); i++) {
@@ -54,8 +72,9 @@ void TelemetryView::addField(GUI_FIELD f, QLayout* layout) {
         
     }
     QLabel* l = new QLabel(parts.join(" ")+" : ");
-    l->setStyleSheet(QString("color: %1;").arg(col::primary));
+    l->setStyleSheet(QString("color: %1;background: transparent;").arg(col::primary));
     QWidget *wrapper = new QWidget;
+    wrapper->setStyleSheet("background-color: transparent;");
     QHBoxLayout* layoutField = new QHBoxLayout(wrapper);
     layoutField->addWidget(l);
     layoutField->addWidget(new DataLabel(f));
